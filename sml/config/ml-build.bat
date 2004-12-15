a6ee60cfdbd123f2e9e138e2e64f73145fbd8cb5
Matthias Blume <blume@tti-c.org>
2004-12-15 17:56:42 +0000

    some bullet-proofing of .bat scripts against spaces in filenames

--- sml/config/ml-build.bat
+++ sml/config/ml-build.bat
@@ -10,14 +10,14 @@ set linkargsfile=XYZ_XXX_LINKARGS
 
 set rare=XYZ_XXX_0123
 
-echo structure %rare% = struct val _ = SMLofNJ.exportFn ("%heap%", %main%) end >%smlfile%
+echo structure %rare% = struct val _ = SMLofNJ.exportFn ("%heap%", %main%) end >"%smlfile%"
 
-echo Group structure %rare% is $/basis.cm %root% %smlfile% >%cmfile%
+echo Group structure %rare% is $/basis.cm "%root%" %smlfile% >%cmfile%
 
 %COMSPEC% /C "%SMLNJ_HOME%\bin\sml.bat @CMbuild %root% %cmfile% %heap% %listfile% %linkargsfile%"
 IF ERRORLEVEL 1 GOTO ERR
 IF NOT EXIST %linkargsfile% GOTO END
-%SMLNJ_HOME%\bin\.run\run.x86-win32.exe @SMLboot=%listfile%
+"%SMLNJ_HOME%\bin\.run\run.x86-win32.exe" @SMLboot=%listfile%
 del %linkargsfile%
 GOTO END
 
