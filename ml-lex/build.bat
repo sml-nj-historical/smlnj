fb087cb2d8b596eaac4a7f203dcfdefa86d0b5b0
Matthias Blume <blume@tti-c.org>
2007-01-31 05:45:02 +0000

    reverted to old tool install scheme

--- ml-lex/build.bat
+++ ml-lex/build.bat
@@ -1,3 +1,1 @@
 ..\bin\ml-build ml-lex.cm ExportLexGen.lexGen ml-lex
-..\bin\sml -m $smlnj/library-install.cm tool/mllex-tool.cm ..\lib
-..\bin\sml -m $smlnj/library-install.cm tool/lex-ext.cm ..\lib
