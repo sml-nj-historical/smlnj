ea70a313129bab3d7c912134c79bdd559f3f650f
Matthias Blume <blume@tti-c.org>
2007-01-31 00:16:13 +0000

    moved CM tool code here

--- ml-lex/build.bat
+++ ml-lex/build.bat
@@ -1,1 +1,3 @@
 ..\bin\ml-build ml-lex.cm ExportLexGen.lexGen ml-lex
+..\bin\sml -m $smlnj/library-install.cm tool/mllex-tool.cm ..\lib
+..\bin\sml -m $smlnj/library-install.cm tool/lex-ext.cm ..\lib
