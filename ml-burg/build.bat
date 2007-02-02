d629837b53c8816aeba835469a74a0ba7a1ca18b
Matthias Blume <blume@tti-c.org>
2007-02-02 21:08:25 +0000

    fixed bug in .bat script

--- ml-burg/build.bat
+++ ml-burg/build.bat
@@ -1,3 +1,4 @@
-..\bin\ml-build ml-burg.cm Main.main ml-burg
-..\bin\sml -m $smlnj/library-install.cm tool/mlburg-tool.cm ..\lib
-..\bin\sml -m $smlnj/library-install.cm tool/burg-ext.cm ..\lib
+@ECHO off
+%COMSPEC% /C "..\bin\ml-build ml-burg.cm Main.main ml-burg"
+%COMSPEC% /C "..\bin\sml -m $smlnj/library-install.cm tool/mlburg-tool.cm ..\lib"
+%COMSPEC% /C "..\bin\sml -m $smlnj/library-install.cm tool/burg-ext.cm ..\lib"
