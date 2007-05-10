82d04168c34360b353caaa023d1debbee2eb220d
John Reppy <jhr@cs.uchicago.edu>
2007-05-10 14:42:51 +0000

    Working on configuration

--- smlnj-lib/Doc/config.sh
+++ smlnj-lib/Doc/config.sh
@@ -6,3 +6,21 @@
 
 find ML-Doc -name "*.mldoc" -print | mk-mldoc-makefile
 
+function mkDirTree {
+  base=$1
+  if test ! -d $base ; then
+    mkdir $base || (echo "unable to create $base"; exit 1)
+  fi
+  for i in ML-Doc/* ; do
+    if test -d $i ; then
+      f=$base/$(basename $i)
+      if test ! -d $f; then
+        echo " creating $f"
+        mkdir $f || (echo "unable to create $f"; exit 1)
+      fi
+    fi
+  done
+}
+
+mkDirTree Info
+mkDirTree HTML
