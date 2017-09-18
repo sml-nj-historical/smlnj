59a9d7108014750ab1ecde369062e66bad28ce56
John Reppy <jhr@cs.uchicago.edu>
2017-09-18 18:01:32 +0000

    add some comments about bug #173

--- sml/runtime/c-libs/posix-process/sleep.c
+++ sml/runtime/c-libs/posix-process/sleep.c
@@ -1,6 +1,13 @@
-/* sleep.c
+/*! \file sleep.c
  *
- * COPYRIGHT (c) 1995 by AT&T Bell Laboratories.
+ * \author John Reppy
+ *
+ * Support for Posix.Process.sleep function
+ */
+
+/*
+ * COPYRIGHT (c) 2017 The Fellowship of SML/NJ (http://www.smlnj.org)
+ * All rights reserved.
  */
 
 #include "ml-base.h"
@@ -12,10 +19,19 @@
 
 /* _ml_P_Process_sleep : int -> int
  *
- * Suspend execution for interval in seconds.
+ * Suspend execution for interval in seconds.  Returns 0 on normal completion, or
+ * else returns the amount of remaining time when signaled.
+ *
+ * TODO: generalize to finer-grain sleeping (bug #173)
  */
 ml_val_t _ml_P_Process_sleep (ml_state_t *msp, ml_val_t arg)
 {
+#if defined(HAS_NANOSLEEP)
+#error nanosleep support not implemented yet
+#elif defined(HAS_USLEEP)
+#error usleep support not implemented yet
+#else
     return INT_CtoML(sleep(INT_MLtoC(arg)));
+#endif
 
 } /* end of _ml_P_Process_sleep */
